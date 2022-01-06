4072826fc544a994e3cb77e96bf6c00ca2b10ff8
Node1 <tasosbarous@gmail.com>
2022-01-06 21:31:12 +0200

    wqd

--- packages/oxygen/.storybook/preview.js
+++ packages/oxygen/.storybook/preview.js
@@ -1,3 +1,10 @@
+import { addDecorator } from "@storybook/react";
+import { withThemesProvider } from "storybook-addon-styled-component-theme";
+import { ThemeProvider } from "styled-components";
+
+// const themes = [theme1, theme2];
+// addDecorator(withThemesProvider(themes), ThemeProvider);
+
 export const parameters = {
     actions: {argTypesRegex: "^on[A-Z].*"},
     controls: {
