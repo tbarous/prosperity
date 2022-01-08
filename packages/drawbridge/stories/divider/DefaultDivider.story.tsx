1ee2afcb37d5913a1d4836aa899aa72b775e940e
Node1 <tasosbarous@gmail.com>
2022-01-08 13:42:47 +0200

    commit-msg

--- packages/drawbridge/stories/divider/DefaultDivider.story.tsx
+++ packages/drawbridge/stories/divider/DefaultDivider.story.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import StorybookWrapper from "@stories/StorybookWrapper";
 import styled from "styled-components";
 import Divider from "@components/divider/Divider";
+import Text from "@components/text/Text";
 
 const StorybookWrapperStyled = styled(StorybookWrapper)`
   margin: 1rem;
 `;
 
-const DefaultDivider = (args: any) => (
+const DefaultDivider = (args: {spacing?: number}) => (
     <StorybookWrapperStyled>
-        <Divider />
+        <Text>Text</Text>
+
+        <Divider spacing={args.spacing}/>
+
+        <Text>Text</Text>
     </StorybookWrapperStyled>
 );
 
 DefaultDivider.args = {
-
+    spacing: 10
 };
 
 export default DefaultDivider;
