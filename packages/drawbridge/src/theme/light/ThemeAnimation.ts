20fd1dea66c5bf0dbc94a754e9197a403acbc5f2
Node1 <tasosbarous@gmail.com>
2022-01-11 21:04:59 +0200

    commit-msg

--- packages/drawbridge/src/theme/light/ThemeAnimation.ts
+++ packages/drawbridge/src/theme/light/ThemeAnimation.ts
@@ -3,7 +3,7 @@ import ThemeAnimationInterface from "@theme/interfaces/ThemeAnimationInterface";
 const ThemeAnimation: ThemeAnimationInterface = {
     drawer: 500,
     snackbar: 500,
-    navbar: 500,
+    navbar: 200,
     carousel: 500,
     modal: 500
 }
