dedd2654bfc78dc5d7f5804956e4e77e05e9f018
Node1 <tasosbarous@gmail.com>
2022-01-15 14:33:38 +0200

    commit-msg

--- packages/drawbridge/src/atoms/Icon.tsx
+++ packages/drawbridge/src/atoms/Icon.tsx
@@ -3,17 +3,15 @@ import {BasicComponentProps, ReactElementOrNull, StyledProps} from "@typings";
 import {IconInterface} from "@icons";
 import styled from "styled-components";
 
-export interface IconStyledProps extends StyledProps {}
-
-const IconStyled = styled.div<IconStyledProps>`
+const IconStyled = styled.div<StyledProps>`
   display: ${p => p.theme.display.flex};
   align-items: ${p => p.theme.alignItems.center};
   justify-content: ${p => p.theme.justifyContent.center};
+`;
 
-  svg {
-    width: ${p => p.theme.dimension.d100};
-    height: ${p => p.theme.dimension.d100};
-  }
+const IconSVG = styled.svg<StyledProps>`
+  width: ${p => p.theme.dimension.d100};
+  height: ${p => p.theme.dimension.d100};
 `;
 
 export interface Props extends BasicComponentProps {
@@ -24,13 +22,26 @@ export interface Props extends BasicComponentProps {
 const Icon: FunctionComponent<Props> = (props: Props): ReactElementOrNull => {
     const {className, icon, onClick} = props;
 
-    if (typeof icon === "undefined") return null;
+    if (!icon) return null;
 
     return (
-        <IconStyled onClick={onClick} className={className}>
-            <svg aria-hidden="true" focusable="false" data-icon={icon.data} role="img" xmlns="http://www.w3.org/2000/svg" viewBox={icon.viewBox}>
-                <path fill={icon.fill} d={icon.d}/>
-            </svg>
+        <IconStyled
+            onClick={onClick}
+            className={className}
+        >
+            <IconSVG
+                aria-hidden="true"
+                focusable="false"
+                data-icon={icon.data}
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox={icon.viewBox}
+            >
+                <path
+                    fill={icon.fill}
+                    d={icon.d}
+                />
+            </IconSVG>
         </IconStyled>
     );
 }
