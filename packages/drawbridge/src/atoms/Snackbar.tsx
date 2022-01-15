b06e657a551e438a94a68d17349545d0114e3d1f
Node1 <tasosbarous@gmail.com>
2022-01-15 14:46:06 +0200

    commit-msg

--- packages/drawbridge/src/atoms/Snackbar.tsx
+++ packages/drawbridge/src/atoms/Snackbar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import {StyledProps} from "@typings";
 import Alert from "@atoms/Alert";
+import Icon from "@atoms/Icon";
+import {px} from "@utils/ThemeUtils";
 
 export interface SnackbarStyledProps extends StyledProps {
     position?: SnackbarPositionVariations
@@ -15,6 +17,14 @@ export enum SnackbarPositionVariations {
 const Snackbar = styled(Alert)<SnackbarStyledProps>`
   top: ${p => p.position === SnackbarPositionVariations.TOP ? p.theme.spacing.s0 : p.theme.spacing.auto};
   bottom: ${p => !p.position || p.position === SnackbarPositionVariations.BOTTOM ? p.theme.spacing.s0 : p.theme.spacing.auto};
+`;
+
+export const SnackbarClose = styled(Icon)<StyledProps>`
+  position: ${p => p.theme.position.absolute};
+  cursor: ${p => p.theme.cursor.pointer};
+  right: ${p => px(p.theme.spacing.s5)};
+  top: ${p => px(p.theme.spacing.s5)};
+  color: ${p => p.theme.color.danger};
 `;
 
 export default Snackbar;
